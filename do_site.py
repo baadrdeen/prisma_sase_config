d8b0ee2f14da49f2c48d5283906d76705a594057
AHARCHI Badr-eddine <66363131+baadrdeen@users.noreply.github.com>
2024-04-08 12:49:10 +0000

    Add files via upload

---
+++ do_site.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from prisma_sase_config.do import go
+
+
+if __name__ == "__main__":
+    go()
+
+
---
+++ pull_site.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from prisma_sase_config.pull import go
+
+if __name__ == '__main__':
+    go()
